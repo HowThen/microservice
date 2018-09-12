b452f03ca85a35d12ae429c856ab56534f2a9c89
HowThen <584973593@qq.com>
2018-09-12 17:55:52 +0800

    microservice2

--- user-thrift-service/src/main/java/cn/tj/user/service/UserServiceImpl.java
+++ user-thrift-service/src/main/java/cn/tj/user/service/UserServiceImpl.java
@@ -2,21 +2,29 @@ package cn.tj.user.service;
 
 import cn.tj.thrift.user.UserInfo;
 import cn.tj.thrift.user.UserService;
+import cn.tj.user.mapper.UserMapper;
 import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService.Iface {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public UserInfo getUserById(int id) throws TException {
-        return null;
+        return userMapper.getUserById(id);
     }
 
     @Override
     public UserInfo getUserByName(String username) throws TException {
-        return null;
+        return userMapper.getUserByName(username);
     }
 
     @Override
     public void registerUser(UserInfo userInfo) throws TException {
-
+        userMapper.registerUser(userInfo);
     }
 }
