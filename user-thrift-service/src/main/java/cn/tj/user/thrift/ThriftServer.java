35ffb09096098d00f7ebf605ed486b8b8cd7e61b
5849673593@qq.com <584973593@qq.com>
2018-09-17 23:21:06 +0800

    5

--- user-thrift-service/src/main/java/cn/tj/user/thrift/ThriftServer.java
+++ user-thrift-service/src/main/java/cn/tj/user/thrift/ThriftServer.java
@@ -2,11 +2,23 @@ package cn.tj.user.thrift;
 
 import cn.tj.thrift.user.UserService;
 import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TTransportException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+@Configuration
 public class ThriftServer {
+
+    @Value("${server.port}")
+    private int serverPort;
 
     @Autowired
     private UserService.Iface userService;
@@ -15,5 +27,19 @@ public class ThriftServer {
     public void startThriftServer() {
         TProcessor processor = new UserService.Processor<>(userService);
 
+        TNonblockingServerSocket socket = null;
+        try {
+            socket = new TNonblockingServerSocket(serverPort);
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+
+        TNonblockingServer.Args  args = new TNonblockingServer.Args(socket);
+        args.processor(processor);
+        args.transportFactory(new TFramedTransport.Factory());
+        args.protocolFactory(new TBinaryProtocol.Factory());
+
+        TServer server = new TNonblockingServer(args);
+        server.serve();
     }
 }
